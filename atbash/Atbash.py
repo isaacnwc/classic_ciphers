06f384f84422c706bee2abc8ac9766edd2df52b2
isaacnwc <isaac.canario@outlook.com>
2026-08-18 01:24:47 -0300

    add lowercase input validation

--- atbash/Atbash.py
+++ atbash/Atbash.py
@@ -10,14 +10,17 @@ def cipher_atbash(text: str) -> str:
         reversed_string(alphabet_lower) + reversed_string(alphabet_upper)
      )
 
-    return text.translate(translation_table)
-    
+    return text.translate(translation_table)     
 
-    
+    #Atbash is self-inverse, the encryption and decryption methods are the same operation because the mapping is simmetrycal.  A - Z, B - Y, C - X, ...
+    #If your input has the value CHICO, calling cipher_atbash will translate to XSRXL.
+    #If your input has the value XSRXL, calling cipher_atbash will translate to CHICO
 
 if __name__ == "__main__":
-    print(cipher_atbash("ABC"))
-    print(cipher_atbash("abc"))
+    print(cipher_atbash("CHICO"))
+    print(cipher_atbash("chico"))
+    print(cipher_atbash("XSRXL"))
+    print(cipher_atbash("xsrxl"))
 
 
 
