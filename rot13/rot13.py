2892a50e872c0c0235f91a890efc47aa2c7dd56a
isaacnwc <isaac.canario@outlook.com>
2026-08-18 23:07:36 -0300

    add rot13 cipher

---
+++ rot13/rot13.py
@@ -0,0 +1,28 @@
+import string
+
+def cipher_rot13(text: str) -> str:
+    alphabet= string.ascii_letters
+    alphabet_13offset_lower= alphabet[13:26] + alphabet [:13]
+    alphabet_13offset_upper = alphabet[39:] + alphabet [26:39]
+
+    translation_table = str.maketrans(
+            
+            alphabet, alphabet_13offset_lower + alphabet_13offset_upper
+            )
+    return text.translate(translation_table)
+
+
+
+if __name__ == "__main__" :
+    print(cipher_rot13("CHICO"))
+    print(cipher_rot13("chico"))
+    print(cipher_rot13("PUVPB"))
+    print(cipher_rot13("puvpb"))
+
+
+
+    
+    
+
+
+
